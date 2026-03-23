af2c9628f173688899b72f2df4d155e0f1160aa9
lucas04501 <lucaspds9@hotmail.com>
2026-03-23 09:57:59 -0300

    for and range e tabuada

---
+++ for-range/for-range.py
@@ -0,0 +1,4 @@
+num = int(input("digite um numero"))
+for i in range(1,11):
+    print(i, "X", num, "=", i * 2)
+#forma de fazer uma multiplicação com o for/range
---
+++ for-range/tabuada.py
@@ -0,0 +1,4 @@
+for j in range(1,11):
+    for i in range(1,11):
+        print(i, "x", j, "=", i * j)
+#forma de fazer uma tabuada com o for/range, onde o primeiro for é para o numero que vai ser multiplicado, e o segundo for é para o numero que vai multiplicar, e o print é para mostrar o resultado da multiplicação.
